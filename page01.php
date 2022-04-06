7061cc28323c251bde430fb6f83819f5490b65d3
Victor Hugo Fuly Silva <victorfuly.vhfs@gmail.com>
2022-04-06 16:40:38 -0300

    Segunda Aula

---
+++ page01.php
@@ -0,0 +1,22 @@
+<?php
+// Cria uma variável
+$carro = 'Fusca';
+$cor_do_carro = 'Azul'
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo $carro ?>
+    <?php echo $cor ?>
+
+</body>
+</html>
