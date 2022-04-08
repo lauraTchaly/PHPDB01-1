0da947a4e00b7df18633e207c1bc8576d92ddc32
Victor Hugo Fuly Silva <victorfuly.vhfs@gmail.com>
2022-04-08 16:46:52 -0300

    Trabalho de PHP com HTML

---
+++ _footer.php
@@ -0,0 +1,13 @@
+</main>
+        <footer>
+            <a href="/" title="Página inicial">
+                <i class="fa-solid fa-house-chimney"></i>
+            </a>
+            <div>&copy; Copyright 2022 Vitugo.</div>
+            <a href="#top" title="Topo da Página">
+                <i class="fa-solid fa-circle-up"></i>
+            </a>
+        </footer>
+    </div>
+</body>
+</html>
---
+++ modelo.php
@@ -0,0 +1,29 @@
+<?php
+//Inclui o cabeçalho da página
+require('_header.php')
+?>
+<section>
+    <h2>Título da Página</h2>
+    <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum asperiores, unde assumenda quam
+         dignissimos repudiandae placeat vel suscipit dolorum quod odio provident quidem odit ab. 
+         Eius odit totam explicabo corrupti!
+    </p>
+    <p>
+         Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint praesentium asperiores delectus. 
+         Reprehenderit tempore quasi esse labore facere accusamus nemo praesentium unde est. 
+         Voluptatem quia incidunt rerum aut doloribus doloremque.
+ </p>
+</section>
+<aside>
+     <h3>Lateral</h3>
+    <p>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+     Perferendis ducimus fugiat sunt velit!
+     </p>
+</aside>
+<?php
+//Inclui o footer da página
+require('_footer.php')
+?>
+        
